631fd4270ebd9971dd636a0ee5a6af2bacbd0dac
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-05-25 11:19:51 -0400

    Create StructureEdge class

---
+++ common/src/main/java/com/github/betterbuiltfool/structure/StructureEdge.java
@@ -0,0 +1,28 @@
+package com.github.betterbuiltfool.structure;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class StructureEdge extends Edge{
+    private final BlockState edgeMaterial;
+    
+    public StructureEdge(Node start, Node end, BlockState material) {
+        super(start, end);
+        edgeMaterial = material;
+    }
+    
+    @Override
+    public int getLength() {
+        return (int) getStartNode().getPosition().distToCenterSqr(getEndNode().getPosition().getCenter());
+    }
+    
+    @Override
+    public int getMaterialCost() {
+        // TODO: Add a filter to remove the irreplaceable blocks
+        return Math.toIntExact(
+                BlockPos.betweenClosedStream(
+                        getStartNode().getPosition(),
+                        getEndNode().getPosition())
+                .count());
+    }
+}

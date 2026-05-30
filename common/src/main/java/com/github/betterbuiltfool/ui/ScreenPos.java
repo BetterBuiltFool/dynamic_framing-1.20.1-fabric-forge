e7c5ff3b0e6b718415fc316f69ddf5329cb7a71e
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-05-30 08:16:45 -0400

    Add ScreenPos toShortString method

--- common/src/main/java/com/github/betterbuiltfool/ui/ScreenPos.java
+++ common/src/main/java/com/github/betterbuiltfool/ui/ScreenPos.java
@@ -1,4 +1,8 @@
 package com.github.betterbuiltfool.ui;
 
 public record ScreenPos(int x, int y) {
+    
+    public String toShortString() {
+        return String.format("[%s, %s]", x, y);
+    }
 }

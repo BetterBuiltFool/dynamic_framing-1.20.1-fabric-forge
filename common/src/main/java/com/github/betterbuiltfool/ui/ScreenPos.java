15d156422836d2720c224dbb6b6d9a5c1f945ad7
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-06-03 22:16:17 -0400

    Remove pointless screenpos class

--- common/src/main/java/com/github/betterbuiltfool/ui/ScreenPos.java
+++
@@ -1,8 +0,0 @@
-package com.github.betterbuiltfool.ui;
-
-public record ScreenPos(int x, int y) {
-    
-    public String toShortString() {
-        return String.format("[%s, %s]", x, y);
-    }
-}

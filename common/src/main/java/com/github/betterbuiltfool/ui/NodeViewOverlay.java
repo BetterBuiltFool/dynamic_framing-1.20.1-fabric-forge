01f0bfffbbbd443e1cf8f1de8c98a24e727d917e
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-06-05 13:30:40 -0400

    Reset overlay class

--- common/src/main/java/com/github/betterbuiltfool/ui/NodeViewOverlay.java
+++ common/src/main/java/com/github/betterbuiltfool/ui/NodeViewOverlay.java
@@ -2,17 +2,10 @@ package com.github.betterbuiltfool.ui;
 
 import com.github.betterbuiltfool.DynamicFraming;
 import com.github.betterbuiltfool.items.FroeTool;
-import com.github.betterbuiltfool.structure.Edge;
-import com.github.betterbuiltfool.structure.JointNode;
 import dev.architectury.event.events.client.ClientGuiEvent;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
-import org.joml.Matrix4f;
-import org.joml.Vector2i;
-import org.joml.Vector3f;
-import org.joml.Vector4f;
+import org.joml.*;
 
 public class NodeViewOverlay {
     
@@ -27,79 +20,18 @@ public class NodeViewOverlay {
             if (client.player == null || client.level == null) {
                 return;
             }
-            
             var item = client.player.getMainHandItem();
             
             if (item.getItem() instanceof FroeTool) {
+                // TODO: add a base class or interface for approving tools
                 DynamicFraming.LOGGER.info("Tool Equipped");
+                
+                
+                var currentNodePos = FroeTool.getFirstPos(item);
+                if (currentNodePos != null) {
+                }
             }
         
         });
-    }
-    
-    private static @NotNull Vector4f worldToNDC(@NotNull Vec3 worldPos) {
-        return worldToNDC(new Vector4f(
-                (float) worldPos.x,
-                (float) worldPos.y,
-                (float) worldPos.z,
-                1.0f
-        ));
-    }
-    
-    private static @NotNull Vector4f worldToNDC(@NotNull Vector4f worldPos){
-        var pos = new Vector4f(worldPos);
-//        return pos.mul(viewMatrix).mul(projection).div(pos.w);
-        return pos.mul(projection).mul(viewMatrix).div(pos.w);
-    }
-    
-    private static Vector2i ndcToScreen(Vector4f ndcPosition) {
-        var pos = new Vector4f(ndcPosition);
-//        pos.div(pos.w);
-        
-        return new Vector2i(
-                (int) ((pos.x + 1) / 2) * screenSize.x,
-                (int) ((1-pos.y) / 2) * screenSize.y
-        );
-    }
-    
-    public static void renderOverlay(GuiGraphics guiGraphics, Minecraft client) {
-    
-    }
-    
-    public static void drawStructure(JointNode startingNode) {
-    
-    }
-    
-    public static void drawEdge(Edge edge) {
-    
-    }
-    
-    private static void drawLine(BlockPos start, BlockPos end) {
-    
-    }
-    
-    private static ScreenPos worldToScreen(BlockPos worldPos, Matrix4f projection) {
-//        Minecraft client = Minecraft.getInstance();
-        
-//        GameRenderer renderer = client.gameRenderer;
-//        Matrix4f projection = renderer.getProjectionMatrix(client.getWindow().getGuiScale());
-        
-        var center = worldPos.getCenter();
-        
-        var vec4WorldPos = new Vector4f(
-                (float) center.x,
-                (float) center.y,
-                (float) center.z,
-                1.0f
-        );
-        
-        var screenPos = projection.transform(vec4WorldPos);
-        
-        return new ScreenPos((int) screenPos.x, (int) screenPos.y);
-        
-    }
-    
-    private static void renderNodeMarker(BlockPos pos) {
-    
     }
 }

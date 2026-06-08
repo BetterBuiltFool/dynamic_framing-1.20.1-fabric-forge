c68ad7d33eff9adf3600e9888835c3fe168adfd1
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-06-08 13:44:40 -0400

    Add basic rendering code

--- common/src/main/java/com/github/betterbuiltfool/ui/NodeViewOverlay.java
+++ common/src/main/java/com/github/betterbuiltfool/ui/NodeViewOverlay.java
@@ -2,11 +2,14 @@ package com.github.betterbuiltfool.ui;
 
 import com.github.betterbuiltfool.DynamicFraming;
 import com.github.betterbuiltfool.items.FroeTool;
-import com.mojang.blaze3d.vertex.PoseStack;
-import dev.architectury.event.events.client.ClientGuiEvent;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.*;
+import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.phys.Vec3;
 import org.joml.*;
 
 public class NodeViewOverlay {
@@ -30,7 +33,33 @@ public class NodeViewOverlay {
             
             var currentNodePos = FroeTool.getFirstPos(item);
             if (currentNodePos != null) {
-            
+                
+                Camera camera = client.getEntityRenderDispatcher().camera;
+                
+                Vec3 renderPos = currentNodePos.getCenter().subtract(camera.getPosition());
+                poseStack.pushPose();
+                Matrix4f pose = poseStack.last().pose();
+                Tesselator tesselator = Tesselator.getInstance();
+                BufferBuilder buffer = tesselator.getBuilder();
+                
+                poseStack.translate((float) renderPos.x(), (float) renderPos.y(), (float) renderPos.z());
+                poseStack.mulPose(camera.rotation());
+                
+                buffer.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION);
+                
+                int width = 8;
+                int height = 8;
+                
+                buffer.vertex(pose, - (float) width / 2, + (float) height / 2, 0).endVertex();
+                buffer.vertex(pose, + (float) width / 2, + (float) height / 2, 0).endVertex();
+                buffer.vertex(pose, + (float) width / 2, - (float) height / 2, 0).endVertex();
+                buffer.vertex(pose, - (float) width / 2, - (float) height / 2, 0).endVertex();
+                
+                RenderSystem.setShader(GameRenderer::getPositionShader);
+                
+                tesselator.end();
+                
+                poseStack.popPose();
             }
         }
         

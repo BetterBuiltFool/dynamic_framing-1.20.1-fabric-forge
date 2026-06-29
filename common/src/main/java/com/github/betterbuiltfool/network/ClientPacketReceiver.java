82857bac792d7fa4ed1ec9e39293b2653ee712e2
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-06-29 13:52:49 -0400

    Add client packet and receiver

---
+++ common/src/main/java/com/github/betterbuiltfool/network/ClientPacketReceiver.java
@@ -0,0 +1,9 @@
+package com.github.betterbuiltfool.network;
+
+import dev.architectury.utils.Env;
+import dev.architectury.utils.EnvExecutor;
+
+public interface ClientPacketReceiver {
+    
+    public static void handleNodeSync(NodeSyncPacket packet) {}
+}

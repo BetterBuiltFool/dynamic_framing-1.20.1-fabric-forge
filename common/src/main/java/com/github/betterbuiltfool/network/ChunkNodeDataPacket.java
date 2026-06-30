cdcabf08962ce5649b67c73b3091a2f8a761f808
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-06-30 14:55:09 -0400

    Add chunk node data packet class

---
+++ common/src/main/java/com/github/betterbuiltfool/network/ChunkNodeDataPacket.java
@@ -0,0 +1,23 @@
+package com.github.betterbuiltfool.network;
+
+import it.unimi.dsi.fastutil.longs.LongArrayList;
+import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
+import it.unimi.dsi.fastutil.longs.LongSet;
+import net.minecraft.network.FriendlyByteBuf;
+
+public class ChunkNodeDataPacket {
+    private final LongSet nodePositions = new LongOpenHashSet();
+    
+    public ChunkNodeDataPacket(long... nodePos) {
+        nodePositions.addAll(LongArrayList.wrap(nodePos));
+    }
+    
+    public ChunkNodeDataPacket(LongSet nodePos) {
+        this(nodePos.toLongArray());
+    }
+    
+    public void encode(FriendlyByteBuf buffer) {
+        buffer.writeInt(nodePositions.size());
+        buffer.writeLongArray(nodePositions.toLongArray());
+    }
+}

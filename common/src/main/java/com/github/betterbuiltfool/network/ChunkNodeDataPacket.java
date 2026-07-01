c1b21242959cf36f83a8d9f7638ae933484d6c96
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-06-30 21:34:51 -0400

    Add blank handle method to ChunkNodeDataPacket

--- common/src/main/java/com/github/betterbuiltfool/network/ChunkNodeDataPacket.java
+++ common/src/main/java/com/github/betterbuiltfool/network/ChunkNodeDataPacket.java
@@ -1,9 +1,15 @@
 package com.github.betterbuiltfool.network;
 
+import com.github.betterbuiltfool.client.ClientLocalNodes;
+import com.github.betterbuiltfool.data.FramedStructureStorage;
+import dev.architectury.networking.NetworkManager;
 import it.unimi.dsi.fastutil.longs.LongArrayList;
 import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
 import it.unimi.dsi.fastutil.longs.LongSet;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.level.ServerPlayer;
+
+import java.util.function.Supplier;
 
 public class ChunkNodeDataPacket {
     private final LongSet nodePositions = new LongOpenHashSet();
@@ -22,5 +28,14 @@ public class ChunkNodeDataPacket {
     
     public void encode(FriendlyByteBuf buffer) {
         buffer.writeLongArray(nodePositions.toLongArray());
+    }
+    
+    public void handle(Supplier<NetworkManager.PacketContext> contextSupplier) {
+        var context = contextSupplier.get();
+        context.queue(() -> {
+            // Do nothing yet
+//            ClientLocalNodes.clear();
+//            ClientLocalNodes.addNodes(this.nodePositions);
+        });
     }
 }

72f2e36851dd512f724b1b12eae00120bc3a849b
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-07-18 21:43:44 -0400

    Add unified distance method for GraphHit

--- common/src/main/java/com/github/betterbuiltfool/structure/GraphHit.java
+++ common/src/main/java/com/github/betterbuiltfool/structure/GraphHit.java
@@ -1,6 +1,9 @@
 package com.github.betterbuiltfool.structure;
 
 public sealed interface GraphHit {
+    
+    double distance();
+    
     record NodeHit(long packedPos, double distance) implements GraphHit {}
     
     record EdgeHit(long posA, long posB, double distance) implements GraphHit {}

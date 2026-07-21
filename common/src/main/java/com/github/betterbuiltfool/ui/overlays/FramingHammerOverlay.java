d3aac6374a2f1073e1d6fe5dd666bcbf6bbb9165
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-07-21 13:14:40 -0400

    Extract node rendering into new method

--- common/src/main/java/com/github/betterbuiltfool/ui/overlays/FramingHammerOverlay.java
+++ common/src/main/java/com/github/betterbuiltfool/ui/overlays/FramingHammerOverlay.java
@@ -3,9 +3,6 @@ package com.github.betterbuiltfool.ui.overlays;
 import com.github.betterbuiltfool.ui.LineRenderer;
 import com.github.betterbuiltfool.ui.NodeMarkerRenderer;
 import com.github.betterbuiltfool.ui.NodeOverlayContext;
-import net.minecraft.client.Camera;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.phys.Vec3;
 
 import java.awt.*;
 
@@ -14,19 +11,23 @@ public class FramingHammerOverlay {
     
     public static void renderOverlay(NodeOverlayContext context) {
         renderEdges(context);
-        var client = context.client;
-        var poseStack = context.poseStack;
-        var positions = context.highlightNodes;
-        
-        Camera camera = client.getEntityRenderDispatcher().camera;
-        
-        for(long node: positions) {
-            Vec3 renderPos = BlockPos.of(node).getCenter().subtract(camera.getPosition());
-            NodeMarkerRenderer.renderNode(poseStack, renderPos, camera);
-        }
+        renderNodes(context);
         
         renderEdges(context);
         
+    }
+    
+    private static void renderNodes(NodeOverlayContext context) {
+        var positions = context.highlightNodes;
+        var nodeRenderer = new NodeMarkerRenderer(context.client, context.poseStack);
+        
+        nodeRenderer.startBatch();
+        
+        for(long node: positions) {
+            nodeRenderer.renderNode(node);
+        }
+        
+        nodeRenderer.finishBatch();
     }
     
     public static void renderEdges(NodeOverlayContext context) {

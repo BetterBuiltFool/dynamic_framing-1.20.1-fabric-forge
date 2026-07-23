555058b3b71d5a9cd816d86f0ccd61cce479c724
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-07-23 05:51:24 -0400

    Move RaycastService to data package

--- common/src/main/java/com/github/betterbuiltfool/data/RaycastService.java
+++ common/src/main/java/com/github/betterbuiltfool/data/RaycastService.java
@@ -1,6 +1,7 @@
-package com.github.betterbuiltfool.structure;
+package com.github.betterbuiltfool.data;
 
-import com.github.betterbuiltfool.data.FramedStructureStorage;
+import com.github.betterbuiltfool.structure.GraphHit;
+import com.github.betterbuiltfool.structure.NodeMap;
 import it.unimi.dsi.fastutil.longs.LongSet;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;

470240b1c6b4de56ee74ea74f6608b9977e7ba41
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-07-23 12:51:35 -0400

    Enforce and order for edge nodes, allowing for edges to be hashable

--- common/src/main/java/com/github/betterbuiltfool/structure/Edge.java
+++ common/src/main/java/com/github/betterbuiltfool/structure/Edge.java
@@ -1,3 +1,11 @@
 package com.github.betterbuiltfool.structure;
 
-public record Edge(long firstPos, long secondPos) {}
+public record Edge(long firstPos, long secondPos) {
+    public Edge {
+        if (firstPos > secondPos) {
+            var temp = firstPos;
+            firstPos = secondPos;
+            secondPos = temp;
+        }
+    }
+}

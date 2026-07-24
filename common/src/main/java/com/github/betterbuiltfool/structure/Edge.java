5b31813fab258d0c5f788af3564231cd2da0371e
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-07-23 21:16:34 -0400

    Add edge intersection method

--- common/src/main/java/com/github/betterbuiltfool/structure/Edge.java
+++ common/src/main/java/com/github/betterbuiltfool/structure/Edge.java
@@ -1,5 +1,7 @@
 package com.github.betterbuiltfool.structure;
 
+import net.minecraft.core.BlockPos;
+
 public record Edge(long firstPos, long secondPos) {
     public Edge {
         if (firstPos > secondPos) {
@@ -7,5 +9,23 @@ public record Edge(long firstPos, long secondPos) {
             firstPos = secondPos;
             secondPos = temp;
         }
+    }
+    
+    public boolean intersectedBy(long targetPos) {
+        int tx = BlockPos.getX(targetPos);
+        int ty = BlockPos.getY(targetPos);
+        int tz = BlockPos.getZ(targetPos);
+        
+        int fx = BlockPos.getX(firstPos);
+        int fy = BlockPos.getY(firstPos);
+        int fz = BlockPos.getZ(firstPos);
+        
+        int sx = BlockPos.getX(secondPos);
+        int sy = BlockPos.getY(secondPos);
+        int sz = BlockPos.getZ(secondPos);
+        
+        return tx >= Math.min(fx, sx) && tx <= Math.max(fx, sx) &&
+               ty >= Math.min(fy, sy) && tx <= Math.max(fy, sy) &&
+               tz >= Math.min(fz, sz) && tx <= Math.max(fz, sz);
     }
 }

66e3bcfff8a09caf6e171887e1f55c82e5893e43
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-07-31 21:28:07 -0400

    Add edge profile for storing edge data

---
+++ common/src/main/java/com/github/betterbuiltfool/blocks/block_entities/EdgeProfile.java
@@ -0,0 +1,6 @@
+package com.github.betterbuiltfool.blocks.block_entities;
+
+import net.minecraft.world.level.block.state.BlockState;
+
+public record EdgeProfile(BlockState material, Size size) {
+}

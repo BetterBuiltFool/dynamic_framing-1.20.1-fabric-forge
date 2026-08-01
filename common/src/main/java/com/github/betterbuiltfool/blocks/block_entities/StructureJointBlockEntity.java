3834b47476512925e5c8a13ca57ff0209ed689d3
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-07-31 21:28:18 -0400

    Add blcok entity for joints

---
+++ common/src/main/java/com/github/betterbuiltfool/blocks/block_entities/StructureJointBlockEntity.java
@@ -0,0 +1,93 @@
+package com.github.betterbuiltfool.blocks.block_entities;
+
+import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class StructureJointBlockEntity extends BlockEntity {
+    
+    private Alignment alignX = Alignment.CENTER;
+    private Alignment alignY = Alignment.CENTER;
+    private Alignment alignZ = Alignment.CENTER;
+    
+    private final Long2ObjectMap<EdgeProfile> edges = new Long2ObjectOpenHashMap<>();
+    
+    public StructureJointBlockEntity(BlockEntityType<?> type,
+                                     BlockPos pos,
+                                     BlockState blockState
+    ) {
+        super(type, pos, blockState);
+    }
+    
+    public void setJointAlignment(Alignment x,
+                                  Alignment y,
+                                  Alignment z
+    ) {
+        this.alignX = x;
+        this.alignY = y;
+        this.alignZ = z;
+        this.sync();
+    }
+    
+    private void sync() {
+        this.setChanged();
+        if (this.level == null || this.level.isClientSide()) {
+            return;
+        }
+        this.level.sendBlockUpdated(this.worldPosition, this.getBlockState(), this.getBlockState(), 3);
+    }
+    
+    //region Serialization
+    
+    @Override
+    protected void saveAdditional(CompoundTag tag) {
+        super.saveAdditional(tag);
+        tag.putInt("JointAX", alignX.ordinal());
+        tag.putInt("JointAY", alignY.ordinal());
+        tag.putInt("JointAZ", alignZ.ordinal());
+        
+        ListTag list = new ListTag();
+        for (var entry : edges.long2ObjectEntrySet()) {
+            CompoundTag entryTag = new CompoundTag();
+            var profile = entry.getValue();
+            entryTag.putLong("TargetNodePos", entry.getLongKey());
+            entryTag.put("Material", NbtUtils.writeBlockState(profile.material()));
+            entryTag.putInt("Size", profile.size()
+                                           .ordinal()
+            );
+        }
+        tag.put("Edges", list);
+    }
+    
+    @Override
+    public void load(CompoundTag tag) {
+        super.load(tag);
+        this.alignX = Alignment.values()[tag.getInt("JointAX")];
+        this.alignY = Alignment.values()[tag.getInt("JointAY")];
+        this.alignZ = Alignment.values()[tag.getInt("JointAZ")];
+        
+        edges.clear();
+        if (!tag.contains("Edges", 9)) {
+            return;
+        }
+        var list = tag.getList("Edges", 10);
+        var blockLookup = BuiltInRegistries.BLOCK.asLookup();
+        
+        for (int i = 0; i < list.size(); i++) {
+            var entry = list.getCompound(i);
+            long nodePos = entry.getLong("TargetNodePos");
+            BlockState material = NbtUtils.readBlockState(blockLookup, entry.getCompound("Material"));
+            Size size = Size.values()[entry.getInt("Size")];
+            
+            edges.put(nodePos, new EdgeProfile(material, size));
+        }
+    }
+    //endregion
+}

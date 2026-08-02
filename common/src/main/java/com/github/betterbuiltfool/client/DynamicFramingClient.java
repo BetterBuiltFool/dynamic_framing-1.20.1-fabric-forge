94f832760a19499afc158a321951cc502affcc9a
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-08-02 15:01:13 -0400

    Add client init

---
+++ common/src/main/java/com/github/betterbuiltfool/client/DynamicFramingClient.java
@@ -0,0 +1,10 @@
+package com.github.betterbuiltfool.client;
+
+public class DynamicFramingClient {
+    
+    public static void init() {
+        SoundEventHook.registerHooks();
+        
+        
+    }
+}

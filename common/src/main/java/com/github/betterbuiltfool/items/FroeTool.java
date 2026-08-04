a0d5322ab2d4b2fb29aef4eb90cf528fb87eb7d2
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-08-04 15:23:39 -0400

    Add animation suppressor method

--- common/src/main/java/com/github/betterbuiltfool/items/FroeTool.java
+++ common/src/main/java/com/github/betterbuiltfool/items/FroeTool.java
@@ -8,8 +8,10 @@ import com.github.betterbuiltfool.ui.overlays.NodeOverlayContextBuilder;
 import com.github.betterbuiltfool.validation.EdgeValidator;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 
 public class FroeTool extends Item implements RendersOverlay, SuppressesEquipAnimation{
@@ -17,6 +19,22 @@ public class FroeTool extends Item implements RendersOverlay, SuppressesEquipAni
     
     public FroeTool(Properties properties) {
         super(properties);
+    }
+    
+    public boolean shouldCauseReequipAnimation(
+            ItemStack oldStack,
+            ItemStack newStack,
+            boolean slotChanged
+    ) {
+        if (slotChanged) {
+            return true;
+        }
+        
+        if (this.shouldSuppressReequip(oldStack, newStack)) {
+            return false;
+        }
+        
+        return oldStack.getItem() != newStack.getItem();
     }
     
     @Override

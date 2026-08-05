bf01c0bb7eac6ed977b0aec9d1d6dda4ba1d406d
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-08-05 06:14:14 -0400

    Refactor how building block validation works

--- common/src/main/java/com/github/betterbuiltfool/validation/ItemValidator.java
+++ common/src/main/java/com/github/betterbuiltfool/validation/ItemValidator.java
@@ -2,18 +2,22 @@ package com.github.betterbuiltfool.validation;
 
 import com.github.betterbuiltfool.config.CommonConfig;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
 
 public class ItemValidator {
     
-    public static boolean validateStructureItem(ItemStack item) {
+    public static @Nullable BlockItem validatedStructureItem(ItemStack item) {
+        if (!(item.getItem() instanceof BlockItem blockItem)) {
+            return null;
+        }
         for (TagKey<Item> tag : CommonConfig.structureMaterialWhitelist.tags()) {
             if (item.is(tag)) {
-                return true;
+                return blockItem;
             }
         }
-        
-        return false;
+        return null;
     }
 }

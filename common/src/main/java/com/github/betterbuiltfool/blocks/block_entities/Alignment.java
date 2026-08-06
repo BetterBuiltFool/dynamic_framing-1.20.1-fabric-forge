a12da25bea6681a1bfcd01899ff7f0bf587ec366
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-08-06 14:02:44 -0400

    Add fromOrdinal lookup for block state enums

--- common/src/main/java/com/github/betterbuiltfool/blocks/block_entities/Alignment.java
+++ common/src/main/java/com/github/betterbuiltfool/blocks/block_entities/Alignment.java
@@ -1,11 +1,14 @@
 package com.github.betterbuiltfool.blocks.block_entities;
 
+// Of note: the ordinals of these value are used for bitwise mapping and unpacking, so altering the order
+// later may have consequences.
 public enum Alignment {
     POSITIVE(1),
     CENTER(0),
     NEGATIVE(-1);
     
     final int alignment;
+    private final static Alignment[] CACHE = Alignment.values();
     
     Alignment(int alignment) {
         this.alignment = alignment;
@@ -19,5 +22,9 @@ public enum Alignment {
         if (value < 0) return NEGATIVE;
         if (value > 0) return POSITIVE;
         return CENTER;
+    }
+    
+    public static Alignment fromOrdinal(int ordinal) {
+        return CACHE[ordinal];
     }
 }
--- common/src/main/java/com/github/betterbuiltfool/blocks/block_entities/Size.java
+++ common/src/main/java/com/github/betterbuiltfool/blocks/block_entities/Size.java
@@ -1,5 +1,7 @@
 package com.github.betterbuiltfool.blocks.block_entities;
 
+// Of note: the ordinals of these value are used for bitwise mapping and unpacking, so altering the order
+// later may have consequences.
 public enum Size {
     QUARTER(0.25f),
     HALF(0.5f),
@@ -7,11 +9,17 @@ public enum Size {
     
     final float thickness;
     
+    private static final Size[] CACHE = Size.values();
+    
     Size(float thickness) {
         this.thickness = thickness;
     }
     
     public float getThickness() {
         return thickness;
+    }
+    
+    public static Size fromOrdinal(int ordinal) {
+        return CACHE[ordinal];
     }
 }

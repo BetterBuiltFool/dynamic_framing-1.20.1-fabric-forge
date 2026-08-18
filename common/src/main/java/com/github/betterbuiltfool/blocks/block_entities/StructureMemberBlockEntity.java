155354a288272dcc1140ab995ada8716151724d0
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-08-18 13:42:44 -0400

    Implement proper client BE updating

--- common/src/main/java/com/github/betterbuiltfool/blocks/block_entities/StructureMemberBlockEntity.java
+++ common/src/main/java/com/github/betterbuiltfool/blocks/block_entities/StructureMemberBlockEntity.java
@@ -1,12 +1,18 @@
 package com.github.betterbuiltfool.blocks.block_entities;
 
+import com.github.betterbuiltfool.DynamicFraming;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class StructureMemberBlockEntity extends BlockEntity {
     
@@ -41,15 +47,26 @@ public class StructureMemberBlockEntity extends BlockEntity {
     @Override
     protected void saveAdditional(CompoundTag tag) {
         super.saveAdditional(tag);
-        NbtUtils.writeBlockPos(jointPos);
+        tag.put("joint_pos", NbtUtils.writeBlockPos(jointPos));
         tag.putString("facing", direction.getName());
     }
     
     @Override
     public void load(CompoundTag tag) {
         super.load(tag);
-        jointPos = NbtUtils.readBlockPos(tag);
+        var jointPosTag = tag.getCompound("joint_pos");
+        jointPos = NbtUtils.readBlockPos(jointPosTag);
         direction = Direction.byName(tag.getString("facing"));
+    }
+    
+    @Override
+    public @NotNull CompoundTag getUpdateTag() {
+        return saveWithoutMetadata();
+    }
+    
+    @Override
+    public @Nullable Packet<ClientGamePacketListener> getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this);
     }
     
     //endregion

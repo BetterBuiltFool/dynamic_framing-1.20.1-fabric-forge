57c1852c60a78048ac575d94b29e4ff79ccc63ca
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-08-20 14:06:11 -0400

    Add alignment and scaling properties to blockstate, and remove horizontal restriction from axis

--- common/src/main/java/com/github/betterbuiltfool/blocks/BeamBlock.java
+++ common/src/main/java/com/github/betterbuiltfool/blocks/BeamBlock.java
@@ -1,5 +1,7 @@
 package com.github.betterbuiltfool.blocks;
 
+import com.github.betterbuiltfool.blocks.block_entities.Alignment;
+import com.github.betterbuiltfool.blocks.block_entities.Size;
 import com.github.betterbuiltfool.blocks.block_entities.StructureMemberBlockEntity;
 import com.github.betterbuiltfool.registry.BlockEntityRegistry;
 import net.minecraft.core.BlockPos;
@@ -15,18 +17,30 @@ import org.jetbrains.annotations.Nullable;
 public class BeamBlock extends FrameBlock {
     public static final String BLOCK_ID = "beam_block";
     
-    public static EnumProperty<Direction.Axis> AXIS = BlockStateProperties.HORIZONTAL_AXIS;
+    public static EnumProperty<Direction.Axis> AXIS = BlockStateProperties.AXIS;
+    public static EnumProperty<Alignment> ALIGNMENT_PRIMARY =
+            EnumProperty.create("alignment_primary", Alignment.class);
+    public static EnumProperty<Alignment> ALIGNMENT_SECONDARY =
+            EnumProperty.create("alignment_secondary", Alignment.class);
+    public static EnumProperty<Size> SCALING =
+            EnumProperty.create("scaling", Size.class);
     
     public BeamBlock(Properties properties) {
         super(properties);
         this.registerDefaultState(this.getStateDefinition()
                                       .any()
-                                      .setValue(AXIS, Direction.Axis.X));
+                                      .setValue(AXIS, Direction.Axis.X)
+                                      .setValue(ALIGNMENT_PRIMARY, Alignment.CENTER)
+                                      .setValue(ALIGNMENT_SECONDARY, Alignment.CENTER)
+                                      .setValue(SCALING, Size.FULL));
     }
     
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(AXIS);
+        builder.add(AXIS)
+                .add(ALIGNMENT_PRIMARY)
+                .add(ALIGNMENT_SECONDARY)
+                .add(SCALING);
     }
     
     @Override

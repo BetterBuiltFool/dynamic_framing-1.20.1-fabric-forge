962dc05638b6aa00797100dddf03143399026271
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-08-25 05:55:29 -0400

    Add camo material to beam block entity

--- common/src/main/java/com/github/betterbuiltfool/blocks/block_entities/StructureMemberBlockEntity.java
+++ common/src/main/java/com/github/betterbuiltfool/blocks/block_entities/StructureMemberBlockEntity.java
@@ -1,10 +1,11 @@
 package com.github.betterbuiltfool.blocks.block_entities;
 
-import com.github.betterbuiltfool.DynamicFraming;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
@@ -18,6 +19,7 @@ public class StructureMemberBlockEntity extends BlockEntity {
     
     public Direction direction;
     public BlockPos jointPos;
+    private @Nullable BlockState material;
     
     public StructureMemberBlockEntity(BlockEntityType<?> type,
                                       BlockPos pos,
@@ -34,12 +36,29 @@ public class StructureMemberBlockEntity extends BlockEntity {
         this.jointPos = jointPos;
     }
     
+    public void setMaterial(@Nullable BlockState material) {
+        this.material = material;
+    }
+    
     public Direction getDirection() {
         return direction;
     }
     
     public BlockPos getJointPos() {
         return jointPos;
+    }
+    
+    public @Nullable BlockState getMaterial() {
+        if (material != null) {
+            return material;
+        }
+        assert level != null;
+        var be = level.getBlockEntity(jointPos);
+        if (be instanceof StructureJointBlockEntity jointEntity) {
+            return jointEntity.getEdgeMaterial(direction);
+        }
+        
+        return null;
     }
     
     //region Serialization
@@ -49,14 +68,21 @@ public class StructureMemberBlockEntity extends BlockEntity {
         super.saveAdditional(tag);
         tag.put("joint_pos", NbtUtils.writeBlockPos(jointPos));
         tag.putString("facing", direction.getName());
+        if (material != null) {
+            tag.put("material", NbtUtils.writeBlockState(material));
+        }
     }
     
     @Override
     public void load(CompoundTag tag) {
         super.load(tag);
         var jointPosTag = tag.getCompound("joint_pos");
+        var blockLookup = BuiltInRegistries.BLOCK.asLookup();
         jointPos = NbtUtils.readBlockPos(jointPosTag);
         direction = Direction.byName(tag.getString("facing"));
+        if (tag.contains("material", Tag.TAG_COMPOUND)) {
+            material = NbtUtils.readBlockState(blockLookup, tag.getCompound("material"));
+        }
     }
     
     @Override
